ee678f6b1980a1055cdbd8704b6f8a152f859a49
Willis Dyson <willisdyson@gmail.com>
2024-05-16 08:42:08 +0100

    Pushing finished code

---
+++ src/MovieSearch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./components/Movie";
+
+const MovieSearchPage = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleMovieSearch = (e) => {
+        e.preventDefault();
+        axios
+            .get("https://api.themoviedb.org/3/search/movie", {
+                params: {
+                    api_key: process.env.REACT_APP_API_KEY,
+                    query: searchQuery,
+                },
+            })
+            .then((response) => {
+                setMovies(response.data.results);
+            });
+    };
+
+    return (
+        <div>
+            <h2>Movie search page</h2>
+            <form onSubmit={handleMovieSearch} style={{ marginBottom: "30px" }}>
+                <input
+                    type="text"
+                    placeholder="Search for a movie"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {movies.map((movie) => (
+                    <Movie key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MovieSearchPage;
